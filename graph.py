302baebfd9455356a5c4c306e29508f6a235f150
dawid <dawid.g.dg@gmail.com>
2024-03-11 21:11:52 +0100

    finish dijkstra alghoritm

--- graph.py
+++ graph.py
@@ -5,8 +5,10 @@
 from collections import defaultdict
 from dataclasses import dataclass
 from decimal import Decimal
+from numbers import Number
 from typing import Any, Callable, Dict, Generator, List, Optional, Tuple
 from queue import SimpleQueue
+from unittest.mock import Mock
 from priority_queue import PriorityQueue
 import pandas as pd
 
@@ -24,6 +26,22 @@ class Timestamp():
         time_int = h*3600 + m * 60 + s
         return cls(time_str, time_int)
 
+    def __eq__(self, other: 'Timestamp') -> bool:
+        return self.time == other.time 
+
+    def __gt__(self, other: 'Timestamp') -> bool:
+        return self.time > other.time
+    
+    def __ge__(self, other: 'Timestamp') -> bool:
+        return self.time >= other.time
+
+    def __le__(self, other: 'Timestamp') -> bool:
+        return self.time <= other.time
+    
+    def __lt__(self, other: 'Timestamp') -> bool:
+        return self.time < other.time
+    
+
 
 @dataclass
 class Node:
@@ -38,10 +56,10 @@ class Node:
         return hash(self) == hash(__value) 
     
     def __str__(self) -> str:
-        return f"{self.stop_name} {self.longitude} {self.latitude}"
+        return self.stop_name
     
     def __repr__(self) -> str:
-        return str(self)
+        return str(self) + f" {self.longitude} {self.latitude}"
 
 @dataclass(unsafe_hash=True)
 class Edge:
@@ -62,22 +80,30 @@ class Graph:
         stops = set(connections[['start_stop']+['start_stop_lat']+['start_stop_lon']].itertuples(index=False, name=None))
         stops.update(set(connections[['end_stop']+['end_stop_lat']+['end_stop_lon']].itertuples(index=False, name=None)))
         
-        # TODO: decide how to handle same name stops
-        # Now each is treated as a sepparate stop
-        # Ideas:
-        # - grouping 
-        # - avarage location (maybe check avarage distance for it???)
+        # group stops to average locations
+        bus_stop_groups = defaultdict(lambda: list())
         for stop in stops:
-            self.graph[f"{stop[0]} {stop[1]} {stop[2]}"] = Node(*stop, [])
-        
+            bus_stop_groups[stop[0]].append((stop[1], stop[2]))
+
+        # average locations
+        for stop_name, locations in bus_stop_groups.items():
+            lon_avg = 0
+            lat_avg = 0
+            for lat, lon in locations:
+                lon_avg += lat
+                lat_avg += lon
+            location_count = len(locations)
+            lon_avg /= location_count
+            lat_avg /= location_count
+            self.graph[stop_name] = Node(stop_name, lat_avg, lon_avg, [])
         
         for connection in connections.to_numpy():  
             company = connection[1]
             line = connection[2]
             dep_time = Timestamp.create_timestamp(connection[3])
             arr_time = Timestamp.create_timestamp(connection[4])
-            start_stop_id = f"{connection[5]} {connection[7]} {connection[8]}"
-            end_stop_id = f"{connection[6]} {connection[9]} {connection[10]}"
+            start_stop_id = connection[5]
+            end_stop_id = connection[6]
 
             start_node = self.graph[start_stop_id]
             end_node = self.graph[end_stop_id]
@@ -123,58 +149,59 @@ class Graph:
 
 
     def dijkstra(self, start_node: Node, end_node: Node, start_time: Timestamp) -> Optional[List[Edge]]:
-        distance_lookup = defaultdict(lambda : (float('inf'), None))
+        distance_lookup: Dict[Node, Number] = defaultdict(lambda : float('inf'))
         prev: Dict[Node, Tuple[Node, Edge]] = defaultdict()
         distance_lookup[start_node] = 0
 
         def dijkstra_distance_strategy(node):
             return distance_lookup[node]
 
-
-        queue = PriorityQueue(dijkstra_distance_strategy, start_node)
-        queue.enqueue(start_node, start_time)
+        queue = PriorityQueue(dijkstra_distance_strategy, start_node, start_time)
         while not queue.is_empty():
-            current = queue.dequeue()
-            if end_node == current:
+            current, arrival = queue.dequeue()
+            if end_node is current:
                 return self.get_path(prev, start_node, end_node)
-            
+
             edges = current.neighbours
-            node_time_lookup = {}
+            node_time_lookup: Dict[Node, Timestamp] = {}
             for edge in edges:
-                if edge.departure_time.time > start_time.time:
-                    if node_time_lookup.get(edge.dest, (float('inf'), None))[0] > edge.arrival_time.time:
-                        node_time_lookup[edge.dest] = edge.arrival_time.time, edge
-            print(node_time_lookup)
-            for node, (arival_time_int, edge) in node_time_lookup.items():
-                print(f"{edge.line} {edge.departure_time.time_str} {current.stop_name} -> {edge.arrival_time.time_str} {node.stop_name}")
-                distance_lookup[node] = arival_time_int
-                prev[node] = current, edge
-                queue.enqueue(node, edge.arrival_time.time)
+                if edge.departure_time >= arrival:
+                    time_lookup = node_time_lookup.get(edge.dest)
+                    if not time_lookup or time_lookup[0] > edge.arrival_time:
+                        node_time_lookup[edge.dest] = edge.arrival_time, edge
+            
+
+            for node, (arival_time, edge) in node_time_lookup.items():
+                if distance_lookup[node] > arival_time.time:
+                    distance_lookup[node] = arival_time.time
+                    prev[node] = current, edge
+                    queue.enqueue(node, arival_time)
+        
+        return None
     
-    def get_path(prev: Dict[Node, Tuple[Edge, Node]], start_node: Node, end_node: Node):
+    def get_path(self, prev: Dict[Node, Tuple[Edge, Node]], start_node: Node, end_node: Node):
         current_node = end_node 
         path = []
         while current_node is not start_node:
             next_node, edge = prev[current_node]
             path.append(edge)
             current_node = next_node 
-        return edge
+        return list(reversed(path))
 
 
 if __name__ == "__main__":
-    # graph = Graph('connection_graph.csv')
+    graph = Graph('connection_graph.csv')
     
-    # def print_edge(node: Node, edge: Edge):
-    #     print(f"{node.stop_name} -> {edge.dest.stop_name}" )
-    # bus_stops = list(graph.graph.keys())
-    # k = bus_stops[0]
-    # k1 = bus_stops[1]
-    # n = graph.graph[k]
-    # n1 = graph.graph[k1]
-    # print(f"Going from {n.stop_name} to {n1.stop_name} at 10:30:10")
-    # gen = graph.dijkstra(n, n1, Timestamp.create_timestamp("10:30:10"))
-    # print(gen)
+    def print_edge(node: Node, edge: Edge):
+        print(f"{node.stop_name} -> {edge.dest.stop_name}" )
+    bus_stops = list(graph.graph.keys())
+    k = "Brücknera"
+    k1 = "ZOO"
+    n = graph.graph[k]
+    n1 = graph.graph[k1]
+    print(f"Going from {n.stop_name} to {n1.stop_name} at 21:05")
+    path = graph.dijkstra(n, n1, Timestamp.create_timestamp("21:05:10"))
     
-    pq = PriorityQueue(lambda n: n.longitude, Node("xx", 2, 3, []))
-    pq.enqueue(Node("sss", 0, 1, []), current_time=None)
-    pq.dequeue()
+    print(n.stop_name, end="")
+    for edge in path:
+        print(f" {edge.departure_time.time_str} -> {edge.arrival_time.time_str} {edge.dest.stop_name} [{edge.line}]")
--- priority_queue.py
+++ priority_queue.py
@@ -1,59 +1,76 @@
-from typing import Callable, Optional, TYPE_CHECKING
+from typing import Callable, Optional, TYPE_CHECKING, Tuple
 
 if TYPE_CHECKING:
     from graph import Node, Timestamp
 
 
 class PriorityQueueNode:
-    def __init__(self, node: 'Node', next = None) -> None:
-        self.node = node 
+    def __init__(self, node: 'Node', arrival_time: 'Timestamp', next = None) -> None:
+        self.node = node
+        self.arrival_time = arrival_time
         self.next: Optional[PriorityQueueNode] = next
-
+    def __str__(self):
+        return f"{str(self.node)} {str(self.next.node) if self.next else 'last'}"
+    
+    
 class PriorityQueue:
     """
         Priority queue implemented as linked list
         Note:
-            - Time complexity for insert O(1)
+            - Time complexity for enqueue O(1)
             - Time complexity for dequeue O(N)
             
     """    
-    def __init__(self, strategy: Callable, node: 'Node'=None) -> None:
-        self._first: PriorityQueueNode =  PriorityQueueNode(node)
-        self.startegy = strategy
+    def __init__(self, strategy: Callable, node: 'Node', arrival_time: 'Timestamp') -> None:
+        self._first: PriorityQueueNode =  PriorityQueueNode(node, arrival_time)
+        self.strategy = strategy
         self._last: PriorityQueueNode = self._first
     
-    def enqueue(self, node: 'Node', current_time: 'Timestamp') -> None: 
-        new_node = PriorityQueueNode(node)
+    def enqueue(self, node: 'Node', arrival_time: 'Timestamp') -> None: 
+        new_node = PriorityQueueNode(node, arrival_time)
         if self._first is None:
             self._first = new_node
-        self._last.next = new_node
+        else:
+            self._last.next = new_node
         self._last = new_node
 
-    def dequeue(self) -> 'Node':
+    def dequeue(self) -> Tuple['Node', 'Timestamp']:
+        min_priority_node = self._first
+        previous_node_to_min = None
         previous_node = self._first
         current = self._first.next
         
-        previous_node_to_max = None
+        
+        previous_node_to_min = None
         min_priority_node = self._first
-        min_priority = self.startegy(min_priority_node.node)
+        min_priority = self.strategy(min_priority_node.node)
+
         while current is not None:
-            current_priority = self.startegy(current.node)
-            if current_priority > min_priority:
+            current_priority = self.strategy(current.node)
+            if current_priority < min_priority:
                 min_priority_node = current
-                min_priority = current_priority
-                previous_node_to_max = previous_node
+                previous_node_to_min = previous_node
 
             previous_node = current
             current = current.next
-            
-        if previous_node_to_max is None:
-            print(self._first.node)
-            self._first = min_priority_node.next
-            print(self._first.node)
-        else:
-            previous_node.next = min_priority_node.next
 
-        return min_priority_node.node
+        if previous_node_to_min is None:
+            self._first = min_priority_node.next
+        else:
+            previous_node_to_min.next = min_priority_node.next
+        
+        if min_priority_node is self._last:
+            self._last = previous_node_to_min 
+
+        return min_priority_node.node, min_priority_node.arrival_time
+    
+    def print_queue(self) -> None:
+        current = self._first
+        print('[ ', end='')
+        while current is not None:
+            print(f"({str(current.node)})", end=', ')
+            current = current.next
+        print(' ]')
     
     def is_empty(self):
         return self._first is None
